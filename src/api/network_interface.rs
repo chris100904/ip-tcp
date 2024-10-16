32c2e9a061cb213032a8403392e37248e363bbf1
chris100904 <73714518+chris100904@users.noreply.github.com>
2024-10-15 23:41:14 -0400

    Added TODOs in `network_interface.rs` and `host.rs`, and restructured both files generally
    The restructuring revolves around having the UDP socket be located in each interface. Each interface will have:
    - udp socket
    - listening thread
    - separate function for sending data
    - pointer to parent device
    
    When receiving data, the interface should process that by sending it up to the parent device to see what should be done with the packet. The listening thread should therefore consistently be running in a loop. The socket should be locked and unlocked during specific send requests (REVIEW THIS).
    
    The `host.rs` changes revolve around the changes to the network interface implementation as well, as there is no need for udp sockets in that file. Similar changes should be made to `router.rs` as well.

--- src/api/network_interface.rs
+++ src/api/network_interface.rs
@@ -1,25 +1,55 @@
+use std::net::{UdpSocket, SocketAddr};
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
+
 pub struct NetworkInterface {
-    // Fields for network interface (e.g., port, socket)
+    parent_device: Box<dyn Device>,
+    udp_socket: Arc<Mutex<UdpSocket>>,
+    listening_thread: thread::JoinHandle<()>,
+}
+
+pub trait Device {
+    fn send_data(&self, destination_ip: Ipv4Addr, data: &[u8]) -> Result<(), std::io::Error>;
+    fn receive_packet(&mut self) -> Result<Vec<u8>, std::io::Error>;
 }
 
 impl NetworkInterface {
-    pub fn initialize_interface(port: u16) -> Result<Self, Error> {
-        // Initialize the network interface with the specified port
+    pub fn new(parent_device: Box<dyn Device>, udp_socket: UdpSocket) -> NetworkInterface {
+        let udp_socket = Arc::new(Mutex::new(udp_socket));
+        let listening_thread = thread::spawn(move || {
+            listen_for_packets(udp_socket);
+        });
+        NetworkInterface {
+            parent_device,
+            udp_socket,
+            listening_thread,
+        }
     }
 
-    pub fn send_data(&self, destination_ip: IpAddr, destination_port: u16, data: &[u8]) -> Result<(), Error> {
-        // Send data to the specified destination
+    fn listen_for_packets(udp_socket: Arc<Mutex<UdpSocket>>) {
+        loop {
+            let mut buf = [0; 1500];
+            match udp_socket.lock().recv_from(&mut buf) {
+                Ok((size, _)) => {
+                    // Process the packet
+                    // maybe send up to the parent device
+                    send_to_parent(&buf[..size]);
+                    println!("Received packet of size {}", size);
+                }
+                Err(e) => {
+                    eprintln!("Error receiving packet: {}", e);
+                }
+            }
+        }
     }
 
-    pub fn receive_packet(&self) -> Result<Packet, Error> {
-        // Receive a packet and return it
+    pub fn send_data(&self, destination_ip: Ipv4Addr, data: &[u8]) -> Result<(), std::io::Error> {
+        self.udp_socket.lock().send_to(data, 0, &format!("{}:0", destination_ip))?;
+        Ok(())
     }
 
-    pub fn encapsulate_packet(&self, packet: &Packet) -> Vec<u8> {
-        // Encapsulate the packet for transmission
-    }
-
-    pub fn decapsulate_packet(&self, raw_data: &[u8]) -> Result<Packet, Error> {
-        // Decapsulate raw data into a Packet struct
+    // TODO: implement this function to send up to the parent device
+    fn send_to_parent(&self, data: &[u8]) -> Result<(), std::io::Error> {
     }
 }
--- src/bin/host.rs
+++ src/bin/host.rs
@@ -2,6 +2,7 @@ use std::net::{IpAddr, Ipv4Addr, UdpSocket};
 use etherparse::{Ipv4HeaderSlice, Ipv4Header};
 use crate::packet::Packet;
 
+// TODO: Update the Host so that it has a parameter with a specific interface. This would mean that UdpSocket is unnecessary here
 #[derive(Debug)]
 pub struct Host {
     pub ip_addr: Ipv4Addr,
@@ -10,17 +11,20 @@ pub struct Host {
 }
 
 impl Host {
+    // TODO: intiialize the interface as part of the Host
     pub fn new(ip_addr: Ipv4Addr, mac_addr: [u8; 6]) -> Host {
         let socket = UdpSocket::bind(format!("{}:0", ip_addr)).unwrap(); // bind to IP
         Host { ip_addr, mac_addr, socket }
     }
 
+    // TODO: update this function so that Host calls the send function from the interface
     pub fn send_data(&self, destination_ip: IpAddr, data: &[u8]) -> Result<(), std::io::Error> {
         let packet = Packet::new(self.ip_addr, destination_ip, 17, data.to_vec());
         self.socket.send_to(&packet.to_bytes(), 0)?;
         Ok(())
     }
 
+    // TODO: Probably delete this
     pub fn receive_packet(&mut self) {
         let mut buf = [0; 1500];
         match self.socket.recv_from(&mut buf) {
@@ -33,11 +37,21 @@ impl Host {
         }
     }
 
+    // TODO: IP FORWARDING
+    pub fn process_local_packet(&mut self, packet: Packet) {
+        
+    }
+
+
+    // TODO: Update this (should be called when we receive packet down to up from the interface)
     pub fn process_packet(&mut self, raw_data: &[u8]) {
         if let Ok(packet) = Packet::parse_ip_packet(raw_data) {
             println!("Received packet from {}: {:?}", packet.src_ip, packet);
             // Handle the packet
         }
+        // local? 
+
+        // not local
     }
 }
 
